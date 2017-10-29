5b1b0b17fdebfe44083cb0b954a5960baeb4d31f
JosepRIvaille <josep.de.cid@gmail.com>
2017-10-29 14:02:42 +0100

    Improve Haskell exercice

--- Haskell/Jutge/Lab3/P37072.hs
+++ Haskell/Jutge/Lab3/P37072.hs
@@ -13,7 +13,15 @@ equal Empty Empty = True
 equal (Node x1 l1 r1) (Node x2 l2 r2) = (x1 == x2) && (equal l1 l2) && (equal r1 r2)
 equal _ _ = False
 
--- isomorphic :: Eq a => Tree a -> Tree a -> Bool
+isomorphic :: Eq a => Tree a -> Tree a -> Bool
+isomorphic Empty Empty = True
+isomorphic (Node x1 l1 r1) (Node x2 l2 r2)
+    | x1 == x2  = firstMutation || secondMutation
+    | otherwise = False
+    where
+        firstMutation = isomorphic l1 l2 && isomorphic r1 r2
+        secondMutation = isomorphic l1 r2 && isomorphic r1 l2
+isomorphic _ _ = False
 
 preOrder :: Tree a -> [a]
 preOrder Empty = []
@@ -28,6 +36,42 @@ inOrder Empty = []
 inOrder (Node x l r) = (inOrder l) ++ [x] ++ (inOrder r)
 
 breadthFirst :: Tree a -> [a]
-breadthFirst (Node x Empty Empty) = []
-breadthFirst t@(Node x l r) = root t ++ root l ++ root r ++ breadthFirst l ++ breadthFirst r
-    where root (Node r _ _) = [r]
+breadthFirst Empty = []
+breadthFirst (Node x l r) = [x] ++ breadthFirst' l r
+
+breadthFirst' :: Tree a -> Tree a -> [a]
+breadthFirst' Empty Empty = []
+breadthFirst' (Node x1 l1 r1) (Node x2 l2 r2) = [x1, x2] ++ first ++ second
+    where
+        first = breadthFirst' l1 r1
+        second = breadthFirst' l2 r2
+
+build :: Eq a => [a] -> [a] -> Tree a
+build _ [] = Empty
+build (x:pre) ino = Node x (build preLeft inoLeft) (build preRight inoRight)
+    where
+        preLeft = intersect pre inoLeft
+        preRight = intersect pre inoRight
+        splitIno = splitVal x ino
+        inoLeft = fst splitIno
+        inoRight = snd splitIno
+
+splitVal :: Eq a => a -> [a] -> ([a], [a])
+splitVal _ [] = ([], [])
+splitVal e l = (leftPart, rightPart)
+    where
+        leftPart = takeWhile (\x -> x /= e) l
+        tailList = dropWhile (\x -> x /= e) l
+        rightPart = if tailList == [] then [] else tail tailList
+
+intersect :: Eq a => [a] -> [a] -> [a]
+intersect [] _ = []
+intersect _ [] = []
+intersect l1 l2 = [e | e <- l1, any (== e) l2]
+
+overlap :: (a -> a -> a) -> Tree a -> Tree a -> Tree a
+overlap _ Empty Empty = Empty
+overlap _ Empty x = x
+overlap _ x Empty = x
+overlap f (Node x1 l1 r1) (Node x2 l2 r2) = applyF
+    where applyF = Node (f x1 x2) (overlap f l1 l2) (overlap f r1 r2)
